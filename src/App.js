b396372db8353d8a5e79ef763182a4b4bbc6add7
John Kraus <john.kraus@redhorsecorp.com>
2020-04-25 13:57:38 -0400

    lesson 71, starting the container

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          BASIC RULES!!!
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-
+/* I changed the code in App.js, so now this test fails
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+*/
+
+test('renders basic rules link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/basic rules/i);
   expect(linkElement).toBeInTheDocument();
 });
